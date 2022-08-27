a51c21d7b5052d4d3f2474787b0d34ff2a2d8867
kalyan-karnati <karnatik97@gmail.com>
2022-08-27 16:19:12 -0400

    Adding code and setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="Dimensions of Shapes",
+    version="1.0",
+    author="CSC510-Software Engineering HomeWork Group 42",
+    install_requires=[
+        'math',
+        'time',
+      ],
+)
