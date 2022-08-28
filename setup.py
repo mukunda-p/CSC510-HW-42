a2b782b9fe184fda1203254add7537d0b4649423
kalyan-karnati <karnatik97@gmail.com>
2022-08-28 12:06:23 -0400

    Modifying name in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 
 setup(
-    name="Dimensions of Shapes",
+    name="Math Functions",
     version="1.0",
     author="CSC510-Software Engineering HomeWork Group 42",
     install_requires=[
         'math',
-        'time',
       ],
 )
