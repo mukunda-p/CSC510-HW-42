35879ddd9dd32a2039daee84ac06348f27071544
kalyan-karnati <karnatik97@gmail.com>
2022-08-28 22:04:28 -0400

    Added Test cases

--- test/test_math_methods.py
+++ test/test_math_methods.py
@@ -1,14 +1,22 @@
-import sys
-from code.math_methods import cube,square,factorial,exp
+import math
+from code.math_methods import *
 
 def test_cube():
-    count=1
-    while count<=100:
-        assert cube(count)==count**3
-        count=count+1
+    assert cube(-2)==(-2)**3
+    assert cube(0)==0**3
+    assert cube(2)==2**3
 
 def test_square():
-    count=1
-    while count<=100:
-        assert square(count)==count**2
-        count=count+1
+    assert square(-2)==(-2)**2
+    assert square(0)==0**2
+    assert square(2)==2**2
+
+def test_factorial():
+    assert factorial(0)==math.factorial(0)
+    assert factorial(2)==math.factorial(2)
+    assert factorial(100)==math.factorial(100)
+
+def test_exp():
+    assert exp(1,2)==math.pow(1,2)
+    assert exp(2,3)==math.pow(2,3)
+    assert exp(100,3)==math.pow(100,3)
