a889ff4401288da3ce17cb88170b0def9cae432b
kalyan-karnati <59049329+kalyan-karnati@users.noreply.github.com>
2022-08-29 01:07:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ from setuptools import setup,find_packages
 setup(
     name="Math Functions",
     version="1.0",
-    author="CSC510-Software Engineering HomeWork Group 42",
+    author="CSC510-Software Engineering HomeWork Group 42, Kalyan Karnati, Mukunda Varma Pericherla, Pranavi Sharma Sanganabhatla, Saketh Vangala, Srihitha Reddy Kaalam",
     packages=find_packages()
+    description='Home work',
+    author_email="mperich@ncsu.edu",
+    packages=find_packages(),
+    tests_require=['pytest'],
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+      ],
+    license='MIT',
+    install_requires=['python', 'pytest'],
 )
